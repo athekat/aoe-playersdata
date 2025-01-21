9cacf3c67f2a5a0ee5525e30d9857d0154f6c7ba
Alan <63990513+athekat@users.noreply.github.com>
2025-01-21 18:38:21 -0300

    Update getPlayerInfo.py

--- getPlayerInfo.py
+++ getPlayerInfo.py
@@ -8,8 +8,8 @@ today = datetime.datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
 
 # Define API endpoints for each player
 player_urls = {
-    "dicopato_1": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561198118459931%22]",
-    "dicopato_2": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561199195740571%22]",
+    "dicopato": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561198118459931%22]",
+    "dicopatito": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561199195740571%22]",
     "sir_monkey": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561198163778606%22]",
     "nanox": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561198191637438%22]",
     "alanthekat": "https://aoe-api.worldsedgelink.com/community/leaderboard/GetPersonalStat?title=age2&profile_names=[%22/steam/76561198153797281%22]",
